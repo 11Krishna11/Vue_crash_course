6673929c76ec720fbf17b682350a6d1564362e43
Coding_Basics <67582752+CodingBasics698@users.noreply.github.com>
2020-10-05 21:33:26 +0530

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a todo list app v1.0.0. It is part of the vue crash course on youtube by Traversy Media.</p>
+  </div>
+</template>
