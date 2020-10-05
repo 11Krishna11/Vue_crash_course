c6f156fd2da00fd6a01a0f806a39ed615c910a72
Coding_Basics <67582752+CodingBasics698@users.noreply.github.com>
2020-10-05 21:34:27 +0530

    Add files via upload

---
+++ dist/js/about.22a8f283.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},n=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("About")]),s("p",[t._v("This is a todo list app v1.0.0. It is part of the vue crash course on youtube by Traversy Media.")])])}],u=s("2877"),o={},r=Object(u["a"])(o,a,n,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.22a8f283.js.map
